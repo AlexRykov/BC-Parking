25b8801eaa740ff03eccdae48538258f36a772a4
AlexRykov1 <rykov818@gmail.com>
2023-01-11 12:21:25 +0300

    2 navigation commit

--- app/src/main/java/com/example/bc_parking/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/bc_parking/presentation/ChooseLevelFragment.kt
@@ -1,4 +1,53 @@
 package com.example.bc_parking.presentation
 
-class ChooseLevelFragment {
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class ChooseLevelFragment: Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding:FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            buttonTest.setOnClickListener {
+                launchLevel(Level.TEST)
+            }
+            buttonEasy.setOnClickListener {
+                launchLevel(Level.EASY)
+            }
+            buttonMedium.setOnClickListener {
+                launchLevel(Level.MEDIUM)
+            }
+            buttonHard.setOnClickListener {
+                launchLevel(Level.HARD)
+            }
+        }
+    }
+
+    private fun launchLevel(level: Level){
+        findNavController().navigate(ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(level))
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
